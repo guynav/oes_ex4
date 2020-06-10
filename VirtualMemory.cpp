4ca7018c5c4dc49216609ae5e7ea2947570cca7d
toozig <1tvcveybv9>
2020-06-10 19:17:13 +0300

    finished the getNewFrame w/o cleaning frame

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -2,10 +2,16 @@
 #include "PhysicalMemory.h"
 #include <math.h>
 #include <iostream>
-
+#include <cstdlib>
 #define OFF_SET_SIZE log2(PAGE_SIZE)
 #define ERROR_MSG  "system error:"
 #define BIT64 64
+
+
+uint64_t getPAdreess(uint64_t pageNumber);
+
+void checkVMDist(uint64_t &maxDistAdd, uint64_t curVMadd, uint64_t pageNumber);
+
 void clearTable(uint64_t frameIndex) {
     for (uint64_t i = 0; i < PAGE_SIZE; ++i) {
         PMwrite(frameIndex * PAGE_SIZE + i, 0);
@@ -56,17 +62,77 @@ uint64_t isEmpty(uint64_t frame)
     {
         idx = frame * PAGE_SIZE + i;
         PMread(idx, &val);
-        maxVal =  std::max((uint64_t) val, maxVal);
+        if(val) {return frame;}
     }
-    return maxVal;
+    return 0;
+}
+
+
+uint64_t getNewFrameHelper(uint64_t curFrame, uint64_t depth, uint64_t &maxDistAdd, uint64_t curVMadd, uint64_t &maxFrame, uint64_t pageNumber)
+{
+    maxFrame = curFrame > maxFrame ? curFrame : maxFrame;
+    if (depth == log2(NUM_FRAMES))
+    {
+        checkVMDist(maxDistAdd, curVMadd, pageNumber);
+        return 0;
+    }
+    word_t nextAdd;
+    uint64_t idx;
+    for (int i = 0; i < PAGE_SIZE; ++i)
+    {
+        idx = curFrame * PAGE_SIZE + i;
+        PMread(idx, &nextAdd);
+        if ((uint64_t) nextAdd)
+        {
+            curVMadd = curVMadd << (int) log2(PAGE_SIZE);
+            curVMadd += i;
+            return getNewFrameHelper((uint64_t) nextAdd, depth + 1,
+                                     maxDistAdd, curVMadd, maxFrame, pageNumber);
+        }
+    }
+    return curFrame;
+
+}
+
+void checkVMDist(uint64_t &maxDistAdd, uint64_t curVMadd, uint64_t pageNumber) {
+    if( curVMadd) {
+        int cur_calc = abs((int) (pageNumber - maxDistAdd));
+        uint64_t cur = std::min(cur_calc, (int) NUM_FRAMES - cur_calc);
+        int checked_calc = abs((int) (pageNumber - curVMadd));
+        uint64_t checked = std::min(checked_calc, (int) NUM_FRAMES - checked_calc);
+        maxDistAdd = !curVMadd || cur < checked ? maxDistAdd : curVMadd;
+    } else
+    {
+        maxDistAdd = curVMadd;
+    }
 }
 
 /**
- * This function gets a new frame iof needed, does all the cleaning and searching
+ * This function gets a new frame if needed, does all the cleaning and searching
  * @return new frame ready to use
  */
-uint64_t getNewFrame()
+uint64_t getNewFrame(uint64_t pageNumber)
 {
+    uint64_t maxDistAdd = 0, newFrame, maxFrame = 0;
+    word_t nextAdd;
+    for (int i = 0; i < PAGE_SIZE; ++i)
+    {
+        PMread(i, &nextAdd);
+        if ((uint64_t) nextAdd)
+        {
+            if( (newFrame = getNewFrameHelper((uint64_t) nextAdd, 1, maxDistAdd, i, maxFrame, pageNumber )))
+            {
+                return newFrame;
+            }
+        }
+    }
+    if(maxFrame < NUM_FRAMES - 1)
+    {
+        return maxFrame + 1;
+    }
+    return getPAdreess(maxDistAdd);
+
+
     uint64_t maxUsedIDX = 0;
     uint64_t curMax;
 
@@ -83,7 +149,7 @@ uint64_t getNewFrame()
 
     }
     // op 2 unused frame
-    if (maxUsedIDX < NUM_FRAMES)
+    if (maxUsedIDX < NUM_FRAMES && maxUsedIDX) //second condition so it wont be 0
     {
         return maxUsedIDX + 1;
     }
@@ -95,7 +161,7 @@ uint64_t getNewFrame()
  * @param pageNumber the part of the uint64_t that  need to get the address
  * @return the physical memory address
  */
-uint getPAdreess(uint64_t pageNumber)
+uint64_t getPAdreess(uint64_t pageNumber)
 {
 
     //Naive implementation. no edge cases
