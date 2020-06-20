fb11e40f2e277c7d5b3edc751cc752486e9570a8
guyn2 <guy.navon1@mail.huji.ac.il>
2020-06-20 11:54:19 +0300

    added README

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -3,19 +3,13 @@
 #include <math.h>
 #include <cstdlib>
 
-#define OFF_SET_SIZE log2(PAGE_SIZE)
-#define BIT64 64
 #define GET_PA(fatherFrame, offSet) PAGE_SIZE * fatherFrame + offSet
-
-
-
-uint64_t getPAdreess(uint64_t pageNumber, uint64_t offset);
 
 void checkVMDist(uint64_t &maxDistAdd, uint64_t curVMadd, uint64_t pageNumber);
 
 uint64_t getNextLevel(uint64_t pageNumber, int iterNum);
 
-uint64_t getPAdreess2(uint64_t pageNumber);
+uint64_t getPAddress(uint64_t virtualAddress);
 
 /**
  * This functions clears a frame from content
@@ -29,97 +23,11 @@ void clearTable(uint64_t frameIndex)
     }
 }
 
-
-/**
- * Splits virtual Address into the offset and the page number
- */
-void splitVAddress(uint64_t virtualAddress, uint64_t &offset, uint64_t &pageNumber)
-{
-    uint64_t valentina = pow(2, OFFSET_WIDTH) - 1;
-    offset = (virtualAddress & valentina);
-    pageNumber = virtualAddress & ~valentina;
-    pageNumber = pageNumber >> OFFSET_WIDTH;
-}
-
-uint64_t getRowIdx(uint64_t pageNumber, int iterNum)
-{
-    int shift2 = BIT64 - OFFSET_WIDTH - log2(PAGE_SIZE) - iterNum * log2(PAGE_SIZE);
-    uint64_t mask = pow(log2(PAGE_SIZE), 2) - 1;
-    mask = mask << shift2;
-    uint64_t curPageNumber = pageNumber & mask;
-    return curPageNumber >> shift2;
-}
-
-
-void safelyRemoveFather(uint64_t darthVader, uint64_t luke, uint64_t pageNumber, bool toEvict = false)
-{
-    word_t nextAdd;
-    uint64_t idx;
-    for (int i = 0; i < PAGE_SIZE; ++i)
-    {
-        idx = darthVader * PAGE_SIZE + i;
-        PMread(idx, &nextAdd);
-        if ((uint64_t) nextAdd == luke)
-        {
-            if (toEvict)
-            {
-                PMevict(luke ,  pageNumber);
-            }
-            PMwrite(GET_PA(darthVader, i), (word_t) 0);
-            break;
-        }
-    }
-
-}
-
-// DFS on the tree
-void getNewFrameHelper(uint64_t curFrame, uint64_t depth, uint64_t &maxDistAdd, uint64_t curVMadd,
-                       uint64_t &maxFrame, uint64_t pageNumber, uint64_t &emptyFrame, uint64_t father, uint64_t prevFrame, uint64_t offset)
-{
-
-    maxFrame = curFrame > maxFrame ? curFrame : maxFrame;
-    if (depth == TABLES_DEPTH)
-    {
-        checkVMDist(maxDistAdd, curVMadd, pageNumber);
-        return;
-    }
-    word_t nextAdd;
-    uint64_t idx;
-    bool empty = true;
-    for (int i = 0; i < PAGE_SIZE; ++i)
-    {
-        idx = curFrame * PAGE_SIZE + i;
-        PMread(idx, &nextAdd);
-        if ((uint64_t) nextAdd)
-        {
-            empty = false;
-            uint64_t nextVMadd = curVMadd << (int) log2(PAGE_SIZE);
-            nextVMadd += i;
-            getNewFrameHelper((uint64_t) nextAdd, depth + 1,
-                              maxDistAdd, nextVMadd, maxFrame, pageNumber, emptyFrame, father, curFrame, offset);
-            if (emptyFrame)
-            {
-                return;
-            }
-        }
-    }
-    if (empty &&  curFrame != father)
-    {
-        emptyFrame =  curFrame;
-        safelyRemoveFather(prevFrame, curFrame, pageNumber);
-
-    }
-
-//    safelyRemoveFather(father, curFrame);
-//    return curFrame;
-
-}
-
 /**
  * For noption 3 of the getting new frame
- * @param maxDistAdd
- * @param curVMadd
- * @param pageNumber
+ * @param maxDistAdd the address which corresponds to the current max distance
+ * @param curVMadd the address to compare to
+ * @param pageNumber the page number
  */
 void checkVMDist(uint64_t &maxDistAdd, uint64_t curVMadd, uint64_t pageNumber)
 {
@@ -140,18 +48,8 @@ void checkVMDist(uint64_t &maxDistAdd, uint64_t curVMadd, uint64_t pageNumber)
  * @param iterNum the depth in the virtual tree
  * @return the physical address of the next page in the tree
  */
-uint64_t getNextLevel(uint64_t pageNumber, int iterNum)
-{
-    int shift2 = VIRTUAL_ADDRESS_WIDTH - OFFSET_WIDTH - (OFFSET_WIDTH) * (iterNum +
-                                                                          1); //BIT64 - OFFSET_WIDTH - log2(PAGE_SIZE) - iterNum * log2(PAGE_SIZE);SHIFT SHOULD BE ZERO
-    uint64_t valentina = PAGE_SIZE - 1; //not giving the right number
-    valentina = valentina << shift2;
-    uint64_t curPageNumber = pageNumber & valentina;
-    uint64_t idx = curPageNumber >> shift2; //getRowIdx(pageNumber, iterNum);
-    return idx;
-}
 
-uint64_t getNextLevel2(uint64_t pageNumber, int iterNum)
+uint64_t getNextLevel(uint64_t pageNumber, int iterNum)
 {
 
     uint64_t shift, mask, offset;
@@ -161,46 +59,48 @@ uint64_t getNextLevel2(uint64_t pageNumber, int iterNum)
     return offset % PAGE_SIZE;
 }
 
-void safelyRemoveFather2(uint64_t darthVader, uint64_t luke, uint64_t pageNumber, bool toEvict = false)
+/**
+ * This cuntion removes a page from its parent and diconnects the page from the parent
+ * @param father the parent page
+ * @param child the child page
+ * @param pageNumber the number of page to evict
+ * @param toEvict to evict to hard drive or not
+ */
+void safelyRemoveFather(uint64_t father, uint64_t child, uint64_t pageNumber, bool toEvict = false)
 {
     if (toEvict)
     {
-        PMevict(luke  , pageNumber);
+        PMevict(child  , pageNumber);
     }
-    uint64_t offSet = getNextLevel2(pageNumber,TABLES_DEPTH);
-    PMwrite(GET_PA(darthVader, offSet), (word_t) 0);
+    uint64_t offSet = getNextLevel(pageNumber, TABLES_DEPTH);
+    PMwrite(GET_PA(father, offSet), (word_t) 0);
 }
 
-void safelyRemoveFrame2(uint64_t pageNumber, uint64_t targetFrame)
+/**
+ * Helper function for finding a new frame. Recursively iterates over the virtual tree to find a new frame
+ * @param curFrame the current frame
+ * @param depth the depth in the virtual tree
+ * @param maxDistAdd the maximal distance of an address
+ * @param pageNumber the number of the page we are in
+ * @param emptyFrame the number of the current empty frame
+ * @param checkedFrame the frame we are currently checking to replace
+ * @param maxDistAdd the virtual address of the frame with the maximal cyclic distance
+ * @param prevFrame the frame we came from
+ * @param fatheRef reference to the father frame
+ */
+
+void getNewFrameHelper(uint64_t curFrame,
+                       uint64_t depth,
+                       uint64_t &maxDistAdd,
+                       uint64_t curVMadd,
+                       uint64_t &maxFrame,
+                       uint64_t pageNumber,
+                       uint64_t &emptyFrame,
+                       uint64_t checkedFrame,
+                       uint64_t &maxDistFrame,
+                       uint64_t prevFrame,
+                       uint64_t &fatheRef)
 {
-    //Naive implementation. no edge cases
-    uint64_t curAddidx ;
-    word_t curFrame = 0;
-
-    for (int i = 0; i < TABLES_DEPTH ; ++i)
-    {
-        curAddidx = getNextLevel2(pageNumber, i);
-        if(i < TABLES_DEPTH - 1) {
-            PMread(GET_PA(curFrame, curAddidx), &curFrame);
-        }
-    }
-    safelyRemoveFather2((uint64_t) curFrame, targetFrame, pageNumber, true);
-
-}
-
-void getNewFrameHelper2(uint64_t curFrame,
-                        uint64_t depth,
-                        uint64_t &maxDistAdd,
-                        uint64_t curVMadd,
-                        uint64_t &maxFrame,
-                        uint64_t pageNumber,
-                        uint64_t &emptyFrame,
-                        uint64_t checkedFrame,
-                        uint64_t &maxDistFrame,
-                        uint64_t prevFrame,
-                        uint64_t &fatheRefrence)
-{
-
     maxFrame = curFrame > maxFrame ? curFrame : maxFrame;
     if (depth == TABLES_DEPTH)
     {
@@ -208,7 +108,7 @@ void getNewFrameHelper2(uint64_t curFrame,
         if(maxDistAdd == curVMadd )
         {
             maxDistFrame = curFrame;
-            fatheRefrence = prevFrame;
+            fatheRef = prevFrame;
         }
         return;
     }
@@ -222,35 +122,41 @@ void getNewFrameHelper2(uint64_t curFrame,
         {
             empty = false;
             uint64_t nextVMadd = (curVMadd << OFFSET_WIDTH) | i;
-            getNewFrameHelper2((uint64_t) nextAdd, depth + 1,
-                               maxDistAdd, nextVMadd, maxFrame, pageNumber, emptyFrame, checkedFrame, maxDistFrame, curFrame, fatheRefrence);
+            getNewFrameHelper((uint64_t) nextAdd, depth + 1,
+                              maxDistAdd, nextVMadd, maxFrame, pageNumber, emptyFrame, checkedFrame, maxDistFrame,
+                              curFrame, fatheRef);
         }
     }
     if (empty && curFrame != checkedFrame)
     {
-        fatheRefrence = prevFrame;
+        fatheRef = prevFrame;
         emptyFrame =  curFrame;
-        maxDistFrame = getNextLevel2(curVMadd, TABLES_DEPTH);
+        maxDistFrame = getNextLevel(curVMadd, TABLES_DEPTH);
     }
 }
 
-uint64_t getNewFrame2(uint64_t pageNumber, uint64_t curFrame)
+/**
+ * Gets a new frame
+ * @param pageNumber
+ * @param curFrame
+ * @return the new frame number to be used
+ */
+uint64_t getNewFrame(uint64_t pageNumber, uint64_t curFrame)
 {
-
     uint64_t maxDistAdd = pageNumber,maxDistFrame, maxFrame = 0, emptyFrame = 0, fatheRefrence;
     // generate a frame to use
-    getNewFrameHelper2(0, //curFrame
-            0, // depth
-            maxDistAdd, //maxDistAdd
-            0, // curVMadd
-            maxFrame, // maxFrame
-            pageNumber, // pageNumber
-            emptyFrame, //emptyFrame
-            curFrame, //checkedFrame
-                       maxDistFrame, //maxDistFrame
-                       0, // maxDistFrame
-                       fatheRefrence // fatheRefrence
-                       );
+    getNewFrameHelper(0, //curFrame
+                      0, // depth
+                      maxDistAdd, //maxDistAdd
+                      0, // curVMadd
+                      maxFrame, // maxFrame
+                      pageNumber, // pageNumber
+                      emptyFrame, //emptyFrame
+                      curFrame, //checkedFrame
+                      maxDistFrame, //maxDistFrame
+                      0, // maxDistFrame
+                      fatheRefrence // fatheReference
+    );
 
     //option 2 - if not all frames are used, given the max + 1
     if (maxFrame < NUM_FRAMES - 1 && !emptyFrame)
@@ -258,10 +164,11 @@ uint64_t getNewFrame2(uint64_t pageNumber, uint64_t curFrame)
         emptyFrame =  maxFrame + 1;
         clearTable(emptyFrame);
     }
-    // option 3  -
-    else if(!emptyFrame) {
+    // option 3  - find new frame with max cyclical distance
+    else if(!emptyFrame)
+    {
         emptyFrame = maxDistFrame;
-        safelyRemoveFather2(fatheRefrence, emptyFrame, maxDistAdd, true);
+        safelyRemoveFather(fatheRefrence, emptyFrame, maxDistAdd, true);
         clearTable(emptyFrame);
     } else{
         PMwrite(GET_PA(fatheRefrence, maxDistFrame),0);
@@ -270,19 +177,24 @@ uint64_t getNewFrame2(uint64_t pageNumber, uint64_t curFrame)
 
 }
 
-uint64_t getPAdreess2(uint64_t virtualAddress)
+/**
+ * Converts vritual address into a physical address
+ * @param virtualAddress the address to convert
+ * @return the matching physical address
+ */
+uint64_t getPAddress(uint64_t virtualAddress)
 {
 
     uint64_t curAdd = 0, offSet;
     word_t nextAdd;
     for (int i = 0; i < TABLES_DEPTH; ++i)
     {
-        offSet = getNextLevel2(virtualAddress, i); // This is the offset of the current tree level
+        offSet = getNextLevel(virtualAddress, i); // This is the offset of the current tree level
         PMread(GET_PA(curAdd, offSet), &nextAdd);
         if (!(uint64_t) nextAdd)
         {
             uint64_t pageNumber = virtualAddress >> OFFSET_WIDTH;
-             uint64_t newFrame = getNewFrame2(pageNumber, curAdd);
+             uint64_t newFrame = getNewFrame(pageNumber, curAdd);
             PMwrite(PAGE_SIZE * curAdd + offSet, newFrame);
 
 
@@ -295,39 +207,58 @@ uint64_t getPAdreess2(uint64_t virtualAddress)
         }
         curAdd = (uint64_t) nextAdd;
     }
-    offSet = getNextLevel2(virtualAddress, TABLES_DEPTH);
+    offSet = getNextLevel(virtualAddress, TABLES_DEPTH);
     return GET_PA(curAdd, offSet);
 }
 
+/**
+ * Initialize the virtual memory
+ */
 void VMinitialize()
 {
     clearTable(0);
 }
 
+/**
+ * verifies an address is in legal format
+ * @param virtualAddress
+ * @return true is illegal, false otherwise
+ */
 bool isLegalVAddress(uint64_t virtualAddress)
 {
     return virtualAddress >= VIRTUAL_MEMORY_SIZE;
 }
 
+/**
+ * Reads the word from the virtual address into value
+ * @param virtualAddress the virtual address
+ * @param value the value read into
+ * @return 1 on success, 0 on failure
+ */
 int VMread(uint64_t virtualAddress, word_t * value)
 {
     if (isLegalVAddress(virtualAddress))
     {
         return 0;
     }
-    uint64_t PA = getPAdreess2(virtualAddress);
+    uint64_t PA = getPAddress(virtualAddress);
     PMread(PA , value);
     return 1;
 }
 
-
+/**
+ * Writes into the word from the virtual address into value
+ * @param virtualAddress the virtual address
+ * @param value the value written into
+ * @return 1 on success, 0 on failure
+ */
 int VMwrite(uint64_t virtualAddress, word_t value)
 {
     if (isLegalVAddress(virtualAddress))
     {
         return 0;
     }
-    uint64_t PA = getPAdreess2(virtualAddress);
+    uint64_t PA = getPAddress(virtualAddress);
     PMwrite(PA , value);
     return 1;
 }
